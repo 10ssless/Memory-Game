ce89ad326040021809b6ac6ae3d23f29ab4b8bbc
zmulji <znmulji1@gmail.com>
2019-08-25 20:44:54 -0400

    added clicked state to flags

--- memory-game/src/components/FlashCard/index.js
+++ memory-game/src/components/FlashCard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import Wrapper from "../Wrapper";
 
 class FlashCard extends React.Component {
   constructor(props) {
--- memory-game/src/components/Wrapper/index.js
+++ memory-game/src/components/Wrapper/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./style.css";
 
-function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+class Wrapper extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      score: 0, 
+      topScore: 0,
+      dblClick: false
+    }
+  }
+
+  // handleClick() {
+  //   console.log(this.state.score)
+  //   this.setState(state => ({
+  //       score: state.score + 1
+  //     }))
+  // }
+  
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div>
+          <span>score: </span> {this.state.score}
+        </div>
+        {this.props.children}
+      </div>);}
 }
 
 export default Wrapper;

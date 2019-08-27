78798d97ba23580d15cdbd06cd03d11ed9a6a277
zmulji <znmulji1@gmail.com>
2019-08-27 10:11:11 -0400

    Updates

---
+++ precache-manifest.676b0fc3b6e863782d408ee61362589e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dc094a4d83bb457286b67cd940be5d",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "762b73fdf02be134edec",
+    "url": "/Memory-Game/static/css/main.c37cde30.chunk.css"
+  },
+  {
+    "revision": "42a86f091843420d1dd0",
+    "url": "/Memory-Game/static/js/2.41cb4fc0.chunk.js"
+  },
+  {
+    "revision": "762b73fdf02be134edec",
+    "url": "/Memory-Game/static/js/main.91b35e8e.chunk.js"
+  },
+  {
+    "revision": "6b3b4e3c67aa1ec6cb38",
+    "url": "/Memory-Game/static/js/runtime~main.20cf6515.js"
+  }
+]);
